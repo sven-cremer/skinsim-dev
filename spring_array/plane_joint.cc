dfcb55016773c587afeac771326e7e5369b49875
Isura <isura@ieee.org>
2014-02-28 13:49:22 -0600

    Added working impedance controller, parameterized impedance params and added controller params to controller msg

--- spring_array/plane_joint.cc
+++ spring_array/plane_joint.cc
@@ -33,19 +33,25 @@ namespace gazebo
       // ROS Nodehandle
       this->ros_node = new ros::NodeHandle("~");
 
-      std::string para_plane_spring = "/plane_spring";
-      std::string para_plane_damper = "/plane_damper";
-
-      if (!this->ros_node->getParam(para_plane_spring, plane_spring)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_plane_spring.c_str()); }
-      if (!this->ros_node->getParam(para_plane_damper, plane_damper)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_plane_damper.c_str()); }
-
       std::string para_explFctr_Kp = "/plane_explFctr_Kp";
       std::string para_explFctr_Ki = "/plane_explFctr_Ki";
       std::string para_explFctr_Kd = "/plane_explFctr_Kd";
+      if (!this->ros_node->getParam(para_explFctr_Kp, explFctr_Kp)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_explFctr_Kp.c_str()); }
+      if (!this->ros_node->getParam(para_explFctr_Ki, explFctr_Ki)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_explFctr_Ki.c_str()); }
+      if (!this->ros_node->getParam(para_explFctr_Kd, explFctr_Kd)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_explFctr_Kd.c_str()); }
 
-      if (!this->ros_node->getParam(para_explFctr_Kp, kp)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_explFctr_Kp.c_str()); }
-      if (!this->ros_node->getParam(para_explFctr_Ki, ki)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_explFctr_Ki.c_str()); }
-      if (!this->ros_node->getParam(para_explFctr_Kd, kd)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_explFctr_Kd.c_str()); }
+      std::string para_impCtr_Xnom = "/plane_impCtr_Xnom";
+      std::string para_impCtr_K    = "/plane_impCtr_K"   ;
+      std::string para_impCtr_D    = "/plane_impCtr_D"   ;
+      if (!this->ros_node->getParam(para_impCtr_Xnom, impCtr_Xnom)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_impCtr_Xnom.c_str()); }
+      if (!this->ros_node->getParam(para_impCtr_K   , impCtr_K   )){ ROS_ERROR("Value not loaded from parameter: %s !)", para_impCtr_K   .c_str()); }
+      if (!this->ros_node->getParam(para_impCtr_D   , impCtr_D   )){ ROS_ERROR("Value not loaded from parameter: %s !)", para_impCtr_D   .c_str()); }
+
+      std::string para_ctrType    = "/plane_ctrType"   ;
+      if (!this->ros_node->getParam(para_ctrType, ctrType)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_ctrType.c_str()); }
+
+      std::string para_targetForce    = "/plane_targetForce"   ;
+      if (!this->ros_node->getParam(para_targetForce, targetForce)){ ROS_ERROR("Value not loaded from parameter: %s !)", para_targetForce.c_str()); }
 
       this->tactile_sub = this->ros_node->subscribe( "tactile_data", 1, &Plane_Joint::tactileCallback, this );
       this->force_pub = this->ros_node->advertise<spring_array::controllerData>("controller_data", 1);
@@ -66,8 +72,6 @@ namespace gazebo
 
     public: void UpdateJoint()
     {
-      double target_force_ = 0.75;
-
 
       m_lock.lock();
         double sensed_force = sens_force ;
@@ -78,36 +82,38 @@ namespace gazebo
       if( this->model_->GetWorld()->GetSimTime().Double() > 1.0 )
       {
 
-  //      // Impedance control
-  //      double rest_angle = 0;
-  //      double stiffness = 24;
-  //      double damp_coefficient = 10;
-  //      double current_angle = this->joint_->GetAngle(0).Radian();
-  //      double current_force = this->joint_->GetForce(0);
-  //      double current_velocity = this->joint_->GetVelocity(0);
-  //      double current_time = this->model_->GetWorld()->GetSimTime().Double();
-  //      this->joint_->SetForce(0, ( rest_angle - current_angle)*stiffness - damp_coefficient*current_velocity );
+        if( ctrType == 0 )
+        {
+          // Explicit force controller
+          double delT = this->model_->GetWorld()->GetSimTime().Double() - prev_time ;
 
-        // Explicit force controller
-//        double kp = 0.40      ;
-//        double ki = 0.0001    ;
-//        double kd = 1*sqrt(kp);
+          prev_time = this->model_->GetWorld()->GetSimTime().Double();
 
-        double delT = this->model_->GetWorld()->GetSimTime().Double() - prev_time ;
+          double a = targetForce - ground_force;
+          double der_err = ( a - a_prev )/0.001;
+          a_prev   = a ;
+          int_err = int_err + a;
 
-        prev_time = this->model_->GetWorld()->GetSimTime().Double();
+  //        this->joint_->SetForce( 0, a*explFctr_Kp );
+  //        this->joint_->SetForce( 0, a*explFctr_Kp + der_err*explFctr_Kd );
+          // TODO need antiwindup
+          this->joint_->SetForce( 0, a*explFctr_Kp + int_err*explFctr_Ki + der_err*explFctr_Kd);
+  //        this->joint_->SetForce( 0, a*explFctr_Kp + int_err*explFctr_Ki );
+        }
 
-        double a = target_force_ - ground_force;
-        double der_err = ( a - a_prev )/0.001;
-        a_prev   = a ;
-        int_err = int_err + a;
+        if( ctrType == 1 )
+        {
+          // Impedance control
+          double rest_angle       = impCtr_Xnom ;
+          double stiffness        = impCtr_K    ;
+          double damp_coefficient = impCtr_D    ;
 
-
-//        this->joint_->SetForce( 0, a*kp );
-//        this->joint_->SetForce( 0, a*kp + der_err*kd );
-        // TODO need antiwindup
-        this->joint_->SetForce( 0, a*kp + int_err*ki + der_err*kd);
-//        this->joint_->SetForce( 0, a*kp + int_err*ki );
+          double current_angle = this->joint_->GetAngle(0).Radian();
+          double current_force = this->joint_->GetForce(0);
+          double current_velocity = this->joint_->GetVelocity(0);
+          double current_time = this->model_->GetWorld()->GetSimTime().Double();
+          this->joint_->SetForce(0, ( rest_angle - current_angle)*stiffness - damp_coefficient*current_velocity );
+        }
 
         //std::cout << name<<" Current force: "<<current_force<<"\n";
       }
@@ -115,6 +121,17 @@ namespace gazebo
       ctrData.time         = this->model_->GetWorld()->GetSimTime().Double();
       ctrData.force_sensed = sensed_force;
       ctrData.force        = ground_force;
+
+      ctrData.explFctr_Kp  = explFctr_Kp;
+      ctrData.explFctr_Ki  = explFctr_Ki;
+      ctrData.explFctr_Kd  = explFctr_Kd;
+
+      ctrData.impCtr_Xnom  = impCtr_Xnom;
+      ctrData.impCtr_K     = impCtr_K   ;
+      ctrData.impCtr_D     = impCtr_D   ;
+
+      ctrData.ctrType      = ctrType    ;
+      ctrData.targetForce  = targetForce;
 
       force_pub.publish( ctrData );
 
@@ -139,21 +156,27 @@ namespace gazebo
     double int_err    ;
     spring_array::controllerData ctrData;
 
-    // Parameters
-    double plane_spring ;
-    double plane_damper ;
-
     // Time
     double prev_time ;
     double a_prev;
-    boost::mutex m_lock;
+    boost::mutex m_lock ;
 
     // Explicit force controller
-    double kp ;
-    double ki ;
-    double kd ;
+    double explFctr_Kp ;
+    double explFctr_Ki ;
+    double explFctr_Kd ;
 
+    // Impedance controller
+    double impCtr_Xnom ;
+    double impCtr_K    ;
+    double impCtr_D    ;
 
+    // Controller type selection
+    // 0 - explicit force control
+    // 1 - impedance control
+    int ctrType ;
+
+    double targetForce ;
 
   };
 
